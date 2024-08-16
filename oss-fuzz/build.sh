2327546e1281132aaef229c1227f1370e8902115
tesla59 <nishant@heim.id>
2024-08-16 18:21:25 +0000

    i have definetly figured it out now
    Signed-off-by: tesla59 <nishant@heim.id>

--- oss-fuzz/build.sh
+++ oss-fuzz/build.sh
@@ -3,8 +3,4 @@
 go install github.com/AdamKorcz/go-118-fuzz-build@latest
 go get github.com/AdamKorcz/go-118-fuzz-build/testing
 
-# compile_native_go_fuzzer github.com/tesla59/im-so-cooked FuzzReverse fuzz_reverse fuzz
-
-go-118-fuzz-build -o fuzz_my_life.a -func FuzzReverse github.com/tesla59/im-so-cooked
-clang -o fuzz_binary fuzz_my_life.a -fsanitize=fuzzer
-./fuzz_binary -max_total_time=60
+compile_native_go_fuzzer github.com/tesla59/im-so-cooked FuzzReverse fuzz_my_life
