bc3790461f8464535514ba868aa2e364b5c0e9dd
tesla59 <nishant@heim.id>
2024-08-16 11:36:01 +0000

    fuzz my life
    Signed-off-by: tesla59 <nishant@heim.id>

--- oss-fuzz/build.sh
+++ oss-fuzz/build.sh
@@ -1,3 +1,10 @@
 #!/bin/bash -eu
+# Dependencies for compile_native_go_fuzzer
+go install github.com/AdamKorcz/go-118-fuzz-build@latest
 go get github.com/AdamKorcz/go-118-fuzz-build/testing
-compile_native_go_fuzzer github.com/tesla59/im-so-cooked FuzzReverse fuzz_reverse gofuzz
+
+# compile_native_go_fuzzer github.com/tesla59/im-so-cooked FuzzReverse fuzz_reverse fuzz
+
+go-118-fuzz-build -o fuzz_my_life.a -func FuzzReverse github.com/tesla59/im-so-cooked
+clang -o fuzz_binary fuzz_my_life.a -fsanitize=fuzzer
+./fuzz_binary -max_total_time=60
