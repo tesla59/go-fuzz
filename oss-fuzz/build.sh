4a362ac139d9ededec31381cacac246cd35b180f
tesla59 <nishant@heim.id>
2024-08-16 10:38:32 +0000

    no error so i guess progress?
    Signed-off-by: tesla59 <nishant@heim.id>

--- oss-fuzz/build.sh
+++ oss-fuzz/build.sh
@@ -1,2 +1,3 @@
 #!/bin/bash -eu
-compile_go_fuzzer github.com/tesla59/im-so-cooked FuzzReverse fuzz_reverse gofuzz
+go get github.com/AdamKorcz/go-118-fuzz-build/testing
+compile_native_go_fuzzer github.com/tesla59/im-so-cooked FuzzReverse fuzz_reverse gofuzz
